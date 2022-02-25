eb5396a31281d00727398e764a9cf1623c90d536
everyday-special <mlidell@gmaill.com>
2022-02-25 14:58:15 -0800

    removed old commented out code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,6 @@ mod wordle;
 use wordle::wordle_ui::WordleUI;
 
 fn main() {
-    /*let sw = SecretWord::new();
-    sw.print_word();
-    let guess = get_guess();
-    println!("{}", guess);
-    let colors = sw.check_guess(&guess);
-    for (i, col) in colors.iter().enumerate() {
-        match col {
-            TextColor::Green => print!("{}", guess.chars().nth(i).unwrap().to_string().green()),
-            TextColor::Yellow => print!("{}", guess.chars().nth(i).unwrap().to_string().yellow()),
-            TextColor::White => print!("{}", guess.chars().nth(i).unwrap().to_string().white()),
-            TextColor::Black => print!("{}", guess.chars().nth(i).unwrap().to_string().black()),
-        }
-    }
-    println!("");
-    println!("Allowed: {}", sw.word_allowed(&guess));
-    println!("Not allowed: {}", sw.word_allowed(&guess));*/
     let mut ui = WordleUI::new();
     ui.main();
 }
